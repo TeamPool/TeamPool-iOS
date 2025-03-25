720477b78d39747d16b658aa4d5f6352f8be7261
hyeonju <shj040201@naver.com>
2025-03-26 03:21:54 +0900

    [#1] TabBar 추가

---
+++ TeamPool/TeamPool/Feature/MyPage/ViewController/MyPageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MyPageViewController.swift
+//  TeamPool
+//
+//  Created by 성현주 on 3/26/25.
+//
+
+import Foundation
+
+final class MyPageViewController: BaseUIViewController {
+
+    // MARK: - Properties
+
+    // MARK: - UI Components
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+    // MARK: - Custom Method
+
+}
+
+
+
--- TeamPool/TeamPool/Global/Base/BaseTabBarController.swift
+++ TeamPool/TeamPool/Global/Base/BaseTabBarController.swift
@@ -9,5 +9,48 @@ import UIKit
 
 class BaseTabBarController: UITabBarController {
 
-}
+    // MARK: - UI Components
 
+    let homeViewController = HomeViewController()
+    let calendarViewController = CalendarViewController()
+    let myPageViewController = MyPageViewController()
+    let apperance = UITabBarAppearance()
+
+    lazy var homeNavigationViewController = UINavigationController(rootViewController: homeViewController)
+    lazy var calendarNavigationViewController = UINavigationController(rootViewController: calendarViewController)
+    lazy var myPageNavigationController = UINavigationController(rootViewController: myPageViewController)
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setTabBar()
+        setViewController()
+    }
+
+    // MARK: - Custom Method
+
+    private func setTabBar() {
+        tabBar.backgroundColor = .white
+        tabBar.tintColor = .black
+        tabBar.itemPositioning = .automatic
+        let viewControllers: [UIViewController] = [homeNavigationViewController,
+                                                   calendarNavigationViewController,
+                                                   myPageNavigationController]
+        self.setViewControllers(viewControllers, animated: false)
+    }
+
+    private func setViewController() {
+        homeViewController.tabBarItem = setTabbarItem(imageName: "house", selectedImageName: "house.fill")
+        calendarViewController.tabBarItem = setTabbarItem(imageName: "calendar", selectedImageName: "calendar.fill")
+        myPageViewController.tabBarItem = setTabbarItem(imageName: "person", selectedImageName: "person.fill")
+    }
+
+    private func setTabbarItem(imageName: String, selectedImageName: String) -> UITabBarItem {
+        let tabBarItem = UITabBarItem(title: nil,
+                                      image: UIImage(systemName: imageName),
+                                      selectedImage: UIImage(systemName: selectedImageName))
+        return tabBarItem
+    }
+}
