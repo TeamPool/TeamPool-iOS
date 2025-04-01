e8a80f7eb2eabe1ba949505211cea1b4af8e8bb1
hyeonju <shj040201@naver.com>
2025-04-02 03:36:26 +0900

    [#8] 플로팅 버튼 구현

--- TeamPool/TeamPool/Feature/Home/View/HomeView.swift
+++ TeamPool/TeamPool/Feature/Home/View/HomeView.swift
@@ -14,12 +14,26 @@ class HomeView: BaseUIView {
     // MARK: - UI Components
 
     let tableView: UITableView = {
-            let tableView = UITableView()
-            tableView.separatorStyle = .none
-            tableView.showsVerticalScrollIndicator = false
-            tableView.register(PoolCell.self, forCellReuseIdentifier: "poolCell")
-            return tableView
-        }()
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
+        tableView.register(PoolCell.self, forCellReuseIdentifier: "poolCell")
+        return tableView
+    }()
+
+    let floatingButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 28
+        button.layer.masksToBounds = false
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.5
+        button.layer.shadowOffset = CGSize(width: 0, height: 4)
+        button.layer.shadowRadius = 8
+        return button
+    }()
 
     // MARK: - Life Cycle
 
@@ -34,13 +48,19 @@ class HomeView: BaseUIView {
     // MARK: - Custom Method
 
     override func setUI() {
-        self.addSubview(tableView)
+        self.addSubviews(tableView, floatingButton)
 
     }
 
     override func setLayout() {
         tableView.snp.makeConstraints { make in
             make.edges.equalTo(safeAreaLayoutGuide)
+        }
+
+        floatingButton.snp.makeConstraints { make in
+            make.width.height.equalTo(56)
+            make.trailing.equalToSuperview().inset(24)
+            make.bottom.equalTo(safeAreaLayoutGuide).inset(40)
         }
 
     }
