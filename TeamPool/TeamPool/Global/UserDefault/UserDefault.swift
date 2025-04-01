5ff2e62f0382736b4785ca9b3d60c7922540978b
hyeonju <shj040201@naver.com>
2025-04-02 01:53:04 +0900

    [#8] userDefault 추가

---
+++ TeamPool/TeamPool/Global/UserDefault/UserDefault.swift
@@ -0,0 +1,29 @@
+//
+//  UserDefault.swift
+//  TeamPool
+//
+//  Created by 성현주 on 4/2/25.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefault<T> {
+    private let key: String
+    private let defaultValue: T
+
+    init(key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+
+    var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
+
---
+++ TeamPool/TeamPool/Global/UserDefault/UserDefaultHandler.swift
@@ -0,0 +1,19 @@
+//
+//  UserDefaultHandler.swift
+//  TeamPool
+//
+//  Created by 성현주 on 4/2/25.
+//
+
+import Foundation
+
+struct UserDefaultHandler {
+    @UserDefault(key: "accessToken", defaultValue: "")
+    static var accessToken: String
+
+    @UserDefault(key: "refreshToken", defaultValue: "")
+    static var refreshToken: String
+
+    @UserDefault(key: "lectures", defaultValue: "")
+    static var userEmail: String
+}
