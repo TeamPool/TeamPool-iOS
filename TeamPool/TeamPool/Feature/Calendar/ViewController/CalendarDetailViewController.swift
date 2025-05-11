765a78753c5befa4f8fc3b539ac25f452310cd5c
raon-dex <whomi1104@naver.com>
2025-05-11 18:24:44 +0900

    [#15] 달력 만들기 진행

--- TeamPool/TeamPool/Feature/Calendar/ViewController/CalendarDetailViewController.swift
+++ TeamPool/TeamPool/Feature/Calendar/ViewController/CalendarDetailViewController.swift
@@ -1,17 +1,12 @@
-//
-//  CalendarDetailViewController.swift
-//  TeamPool
-//
-//  Created by 성현주 on 4/2/25.
-//
-
-import Foundation
 import UIKit
 
-final class HalfModalViewController: UIViewController {
+final class CalendarDetailViewController: UIViewController {
 
+    // MARK: - Properties
     private let date: Date
+    private let calendarDetailView = CalendarDetailView()
 
+    // MARK: - Initializer
     init(date: Date) {
         self.date = date
         super.init(nibName: nil, bundle: nil)
@@ -21,45 +16,27 @@ final class HalfModalViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private let dateLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 1
-        label.textAlignment = .left
-        return label
-    }()
-
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(dateLabel)
+        view.addSubview(calendarDetailView)
 
-        dateLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(30)
-            $0.leading.equalToSuperview().inset(30)
+        calendarDetailView.snp.makeConstraints { $0.edges.equalToSuperview() }
+
+        // ✅ 핵심: 이벤트 데이터 전달
+        loadEvents(for: date)
+    }
+
+    // MARK: - Data Binding
+    private func loadEvents(for date: Date) {
+        let allEvents = CalendarModel.dummyData()
+        let filteredEvents = allEvents.filter {
+            guard let range = Calendar.current.dateInterval(of: .day, for: date) else { return false }
+            return $0.startDate <= range.end && $0.endDate >= range.start
         }
 
-        setDateLabel(for: date)
-    }
-
-    private func setDateLabel(for date: Date) {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "ko_KR")
-        formatter.dateFormat = "d"
-        let day = formatter.string(from: date)
-
-        formatter.dateFormat = "EEEE"
-        let weekday = formatter.string(from: date)
-
-        let attributed = NSMutableAttributedString()
-        attributed.append(NSAttributedString(
-            string: day + " ",
-            attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .bold)]
-        ))
-        attributed.append(NSAttributedString(
-            string: weekday,
-            attributes: [.font: UIFont.systemFont(ofSize: 16, weight: .regular), .foregroundColor: UIColor.darkGray]
-        ))
-
-        dateLabel.attributedText = attributed
+        calendarDetailView.update(date: date, events: filteredEvents)
     }
 }
+
