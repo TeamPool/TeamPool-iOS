0d533335b3b4273fcf71578434b110cf1be42092
hyeonju <shj040201@naver.com>
2025-06-04 00:55:26 +0900

    [#23] 친구 검색 api 연결

--- TeamPool/TeamPool/Feature/AddPool/FindPeopleModel.swift
+++ TeamPool/TeamPool/Feature/AddPool/FindPeopleModel.swift
@@ -8,15 +8,27 @@
 import Foundation
 
 struct FindPeopleModel {
-    let friendId: Int
+    let friendId: Int?
     let nickname: String
     let studentNumber: String
 
     var name: String { nickname }
+
+    init(friendId: Int?, nickname: String, studentNumber: String) {
+        self.friendId = friendId
+        self.nickname = nickname
+        self.studentNumber = studentNumber
+    }
 
     init(from dto: FriendDTO) {
         self.friendId = dto.friendId
         self.nickname = dto.nickname
         self.studentNumber = dto.studentNumber
     }
+}
+
+
+struct SearchFriendDTO: Codable {
+    let nickname: String
+    let studentNumber: String
 }
